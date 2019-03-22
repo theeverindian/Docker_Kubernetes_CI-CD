99883ac924155d78c887fb97443226cdbd6ac57a
Ranjith Cheguri <theeverindian@gmail.com>
2019-03-22 05:59:11 -0700

    visits node app

---
+++ visits/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var redis = require('redis');
+
+const app = express();
+
+const client = redis.createClient();
+client.set('visitCount',0);
+
+app.get('/',(req,res)=>{
+    client.get('visitCount',(err,visitCount)=>{
+        res.send('Number of visits is : '+visitCount);
+        client.set('visitCount',parseInt(visitCount)+1);
+    })
+})
+
+app.listen(3001,()=>{
+    console.log("listening on port 3001")
+});
